be261b6bceae118e6e7859c35993037b580eea24
Joerg Sigle <info@jsigle.com>
2021-04-06 18:09:40 +0200

    OrderService.java View Artikelauswahl - try catch around java.lang.NullPointerException; removed unresolvable import

--- bundles/ch.elexis.core.data/src/ch/elexis/core/data/service/OrderService.java
+++ bundles/ch.elexis.core.data/src/ch/elexis/core/data/service/OrderService.java
@@ -2,6 +2,9 @@ package ch.elexis.core.data.service;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import ch.elexis.core.model.IOrder;
 import ch.elexis.core.model.IOrderEntry;
 import ch.elexis.core.model.IStockEntry;
@@ -11,20 +14,29 @@ import ch.elexis.data.BestellungEntry;
 import ch.elexis.data.Query;
 import ch.elexis.data.StockEntry;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class OrderService implements IOrderService {
+	private static Logger log = LoggerFactory.getLogger(OrderService.class);
 	
 	public IOrderEntry findOpenOrderEntryForStockEntry(IStockEntry ise){
 		StockEntry se = (StockEntry) ise;
 		Artikel article = se.getArticle();
-		Query<BestellungEntry> qre = new Query<BestellungEntry>(BestellungEntry.class);
-		qre.add(BestellungEntry.FLD_STOCK, Query.EQUALS, se.getStock().getId());
-		qre.add(BestellungEntry.FLD_ARTICLE_TYPE, Query.EQUALS, article.getClass().getName());
-		qre.add(BestellungEntry.FLD_ARTICLE_ID, Query.EQUALS, article.getId());
-		qre.add(BestellungEntry.FLD_STATE, Query.NOT_EQUAL,
-			Integer.toString(BestellungEntry.STATE_DONE));
-		List<BestellungEntry> execute = qre.execute();
-		if (!execute.isEmpty()) {
-			return execute.get(0);
+		//20210406js: avoid java.lang.NullPointerException on showing this view
+		try {
+			Query<BestellungEntry> qre = new Query<BestellungEntry>(BestellungEntry.class);
+			qre.add(BestellungEntry.FLD_STOCK, Query.EQUALS, se.getStock().getId());
+			qre.add(BestellungEntry.FLD_ARTICLE_TYPE, Query.EQUALS, article.getClass().getName());
+			qre.add(BestellungEntry.FLD_ARTICLE_ID, Query.EQUALS, article.getId());
+			qre.add(BestellungEntry.FLD_STATE, Query.NOT_EQUAL,
+				Integer.toString(BestellungEntry.STATE_DONE));
+			List<BestellungEntry> execute = qre.execute();
+			if (!execute.isEmpty()) {
+				return execute.get(0);
+			}
+		} catch(NullPointerException e) {
+			log.error("findOpenOrderEntryForStockEntry(): NullPointerException most probably on article.getClass().getName()");
 		}
 		return null;
 	}
